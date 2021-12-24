0f1a9001cae736516d3c90405194e03a7035ff34
rohit rai <rohitrai.dev@gmail.com>
2021-12-24 11:16:22 +0530

    backend connection done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import app from './app.js'
 import dotenv from "dotenv"
+import { connectDatabse } from './config/databse.js'
+
 dotenv.config({path:"backend/config/config.env"})
+
+connectDatabse()
 
 app.listen(process.env.PORT_NUMBER,()=>{
     console.log(`server is working on http://localhost:${process.env.PORT_NUMBER}`)
