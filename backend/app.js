3cc142e87565c67bf96040f37ffbbafd93208a95
rohit rai <rohitrai.dev@gmail.com>
2021-12-24 14:41:42 +0530

    controllers and services seprated

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ import fs from "fs";
 import { Error } from "./middleware/error.js";
 const app = express();
 app.use(express.json())
-import posts from './routes/prostRoute.js'
-app.use("/api/v1",posts)
+import posts from './routes/postRoute.js'
+app.use("/api/v1/",posts)
 app.use(Error)
 export default app
--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,7 +1,18 @@
-import PostMessage from "../models/postModel.js"
-export const getPosts=(req,res)=>{
-    res.send("this works")
-}
+import { catchAsyncErr } from "../middleware/catchAsyncErrors.js"
+import PostMessage from "../models/postModel.js";
+import { getAllPosts } from "../services/postServices.js";
+export const getPosts=catchAsyncErr(async(req,res,next)=>{
+    const allPostMessages=await getAllPosts();
+    const name="g"
+    res.status(200).json({
+        success:true,
+        name,
+        allPostMessages
+        
+    })
+
+    
+})
 
 export const  createPost=(req,res)=>{
     res.status(200).json({mesaage:"first route"})
